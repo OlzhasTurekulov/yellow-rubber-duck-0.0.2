415ed0a5fc4aec114c9f5fe699f441b3ceab7e4b
oleke94 <oleke.turekulov@gmail.com>
2025-11-01 03:53:35 +0500

    changes

--- src/main/java/framework/web/HttpStatusCode.java
+++
@@ -1,7 +0,0 @@
-package framework.web;
-
-public class HttpStatusCode {
-    public static final int OK = 200;
-    public static final int NOT_FOUND = 404;
-    public static final int INTERNAL_SERVER_ERROR = 500;
-}
